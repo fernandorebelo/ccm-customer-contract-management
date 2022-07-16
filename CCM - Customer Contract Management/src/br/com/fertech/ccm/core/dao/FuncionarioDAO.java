8cb0fa47617e3ea145887d5c4e3e3acc61dc8d22
unisul <unisul@UNS12DT100009.animaedu.intranet>
2022-07-16 10:06:53 -0300

    feat: conexão FuncionarioDAO ao bd

--- CCM - Customer Contract Management/src/br/com/fertech/ccm/core/dao/FuncionarioDAO.java
+++ CCM - Customer Contract Management/src/br/com/fertech/ccm/core/dao/FuncionarioDAO.java
@@ -1,12 +1,39 @@
 package br.com.fertech.ccm.core.dao;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.fertech.ccm.core.dao.connection.ConnectionMySQL;
 import br.com.fertech.ccm.core.entity.FuncionarioEntity;
 
 public class FuncionarioDAO {
 
 	public String salvarFuncionario(FuncionarioEntity funcionario) {
 		System.out.println("Cadastrando no banco de dados...");
-		//TODO implementar cadastro dessa informação no banco de dados
+
+		String sql = "INSERT INTO FUNCIONARIO (NOME_FUNCIONARIO, CARGO_FUNCIONARIO, REGISTRO_FUNCIONARIO) VALUES (?,?,?)";
+		PreparedStatement ps = null;
+		
+		try {
+			ps = ConnectionMySQL.getConnection().prepareStatement(sql);
+			
+			ps.setString(1, funcionario.getNome());
+			ps.setString(2, funcionario.getCargo());
+			ps.setString(3, funcionario.getRegistroProfissional());
+			
+			ps.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
 		return "Funcionário cadastrado.";
 	}
 }
