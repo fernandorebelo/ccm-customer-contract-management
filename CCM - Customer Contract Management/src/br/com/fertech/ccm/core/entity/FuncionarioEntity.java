b26ce840745b7f4bc244e86d7571511b3fe24f24
Fernando Rebelo <fernandorebelo.dev@gmail.com>
2022-07-31 17:00:35 -0300

    feat: adicionado atributo de login e senha em FuncionarioEntity

--- CCM - Customer Contract Management/src/br/com/fertech/ccm/core/entity/FuncionarioEntity.java
+++ CCM - Customer Contract Management/src/br/com/fertech/ccm/core/entity/FuncionarioEntity.java
@@ -8,7 +8,21 @@ public class FuncionarioEntity {
 	private String nome;
 	private String cargo;
 	private String registroProfissional;
+	private String login;
+	private String senha;
 	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 	public long getCodigoFuncionario() {
 		return codigoFuncionario;
 	}
